f5988cad3444026c9c0e52959f8c6fbe708c7900
SantiDee <luis.santiago@tectijuana.edu.mx>
2017-10-25 16:29:45 -0700

    micommit

---
+++ Venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/santidee/Escritorio/proyecto/examen_desarroweb_1/Venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ Venv/carros/views.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+def lago(request):
+    return render(request,"home.html")
